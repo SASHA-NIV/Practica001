9b886f7706b1276906c508b53aec07d803734740
SASHA <SASHA@LAPTOP-K7ENAJFV>
2025-05-19 17:55:03 -0500

    mensajes

---
+++ src/main/java/prueba2/hola2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba2;
+
+/**
+ *
+ * @author SASHA
+ */
+public class hola2 {
+    public static void main(String[] args) {
+        System.out.println("gaaa");
+    }
+}
